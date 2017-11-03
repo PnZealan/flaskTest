9934c6df6de791f17d4b3256ab5cf8708fd695b3
root <zap-@outlook.com>
2017-11-03 09:58:45 +0800

    complete 1st forms

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
+    SubmitField
+from wtforms.validators import DataRequired, Length, Email, Regexp
+from wtforms import ValidationError
+from flask_pagedown.fields import PageDownField
+from ..models import Role, User
 
 
 
 
-class LoginForm(FlaskForm):
-    name = StringField('please input your name .', )
-    
+class NameForm(FlaskForm):
+    name = StringField('输入用户名', validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+class EditProfileForm(FlaskForm):
+    name = StringField('用户名', validators=[Length(0, 64)])
+    location = StringField('位置', validators=[Length(0, 64)])
+    about_me = TextAreaField('我的介绍')
+    submit = SubmitField('提交')
+
+class PostForm(FlaskForm):
+    body = PageDownField("输入一大段文字", validators=[DataRequired()])
+    submit = SubmitField('提交')
+
+class CommentForm(FlaskForm):
+    body = StringField('输入你的评论', validators=[DataRequired()])
+    submit = SubmitField('提交')
