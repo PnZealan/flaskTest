b9c522395cd09ccb6b17748e167ba504d1ac55e0
root <zap-@outlook.com>
2017-11-07 13:49:01 +0800

    comp role

--- app/models.py
+++ app/models.py
@@ -16,6 +16,19 @@ class Permission:
     WRITE = 4
     ADMIN = 16
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = 0
+
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -43,4 +56,4 @@ class User(db.Model):
                                 lazy='dynamic',
                                 cascade='all, delete-orphan')
     comments = db.relationship('Comment', backref='author', lazy='dynamic')
-    	
+    
