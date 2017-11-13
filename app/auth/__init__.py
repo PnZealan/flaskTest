3bc8b32d242d6cb50290bbacf8db7c25d590b31b
root <zap-@outlook.com>
2017-11-13 11:24:48 +0800

    fix

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date    : 2018-06-17 20:52:53
-# @Author  : zap ()
-# @Link    : 
-# @Version : $Id$
-
-
-from flask import Blueprin
+from flask import Blueprint
 
 auth = Blueprin('auth', __name__)
+
+from . import views
